d2dd1c025a83d3920fe2ff077bd3f37f86a9091d
Timothée Haudebourg <timothee.haudebourg@hemisemi.fr>
2022-06-19 12:24:28 +0200

    Add `load` library for loading TreeLDR schemas.

---
+++ load/src/lib.rs
@@ -0,0 +1,270 @@
+use codespan_reporting::term::{
+	self,
+	termcolor::{ColorChoice, StandardStream},
+};
+use std::convert::Infallible;
+use std::path::{
+	Path,
+	PathBuf
+};
+use thiserror::Error;
+use treeldr_syntax as syntax;
+
+pub mod source;
+
+pub use treeldr::{
+	Vocabulary,
+	reporting,
+	vocab::BorrowWithVocabulary
+};
+pub type BuildContext = treeldr_build::Context<source::FileId, syntax::build::Descriptions>;
+
+/// Build all the given documents.
+pub fn build_all(
+	vocabulary: &mut treeldr::Vocabulary,
+	build_context: &mut BuildContext,
+	mut documents: Vec<Document>
+) -> Result<treeldr::Model<source::FileId>, BuildAllError> {
+	build_context.apply_built_in_definitions().unwrap();
+
+	for doc in &mut documents {
+		doc.declare(build_context, vocabulary).map_err(BuildAllError::Declaration)?
+	}
+
+	for doc in documents {
+		doc.build(build_context, vocabulary).map_err(BuildAllError::Link)?
+	}
+
+	let build_context = build_context.simplify(vocabulary).map_err(BuildAllError::Simplification)?;
+	build_context.build(vocabulary).map_err(BuildAllError::Build)
+}
+
+#[derive(Error, Debug)]
+pub enum LoadError {
+	#[error("unsupported MIME type `{0}`")]
+	UnsupportedMimeType(source::MimeType),
+
+	#[error("unrecognized format for file `{0}`")]
+	UnrecognizedFormat(PathBuf),
+
+	#[error("unable to read file `{0}`: {1}")]
+	UnableToRead(PathBuf, std::io::Error)
+}
+
+pub struct TreeLdrDocument {
+	doc: syntax::Document<source::FileId>,
+	local_context: syntax::build::LocalContext<source::FileId>,
+}
+
+impl TreeLdrDocument {
+	fn declare(
+		&mut self,
+		context: &mut BuildContext,
+		vocabulary: &mut Vocabulary,
+	) -> Result<(), syntax::build::Error<source::FileId>> {
+		use treeldr_build::Document;
+		self.doc
+			.declare(&mut self.local_context, context, vocabulary)
+	}
+
+	fn build(
+		mut self,
+		context: &mut BuildContext,
+		vocabulary: &mut Vocabulary,
+	) -> Result<(), syntax::build::Error<source::FileId>> {
+		use treeldr_build::Document;
+		self.doc
+			.relate(&mut self.local_context, context, vocabulary)
+	}
+}
+
+pub enum BuildAllError {
+	Declaration(LangError),
+	Link(LangError),
+	Simplification(<syntax::build::Descriptions as treeldr_build::Simplify<source::FileId>>::Error),
+	Build(treeldr_build::Error<source::FileId>)
+}
+
+impl treeldr::reporting::DiagnoseWithVocabulary<source::FileId> for BuildAllError {
+	fn message(&self, vocabulary: &Vocabulary) -> String {
+		match self {
+			Self::Declaration(e) => e.message(vocabulary),
+			Self::Link(e) => e.message(vocabulary),
+			Self::Simplification(e) => e.message(vocabulary),
+			Self::Build(e) => e.message(vocabulary),
+		}
+	}
+
+	fn labels(
+		&self,
+		vocabulary: &Vocabulary,
+	) -> Vec<codespan_reporting::diagnostic::Label<source::FileId>> {
+		match self {
+			Self::Declaration(e) => e.labels(vocabulary),
+			Self::Link(e) => e.labels(vocabulary),
+			Self::Simplification(e) => e.labels(vocabulary),
+			Self::Build(e) => e.labels(vocabulary),
+		}
+	}
+
+	fn notes(&self, vocabulary: &Vocabulary) -> Vec<String> {
+		match self {
+			Self::Declaration(e) => e.notes(vocabulary),
+			Self::Link(e) => e.notes(vocabulary),
+			Self::Simplification(e) => e.notes(vocabulary),
+			Self::Build(e) => e.notes(vocabulary),
+		}
+	}
+}
+
+pub enum LangError {
+	TreeLdr(syntax::build::Error<source::FileId>),
+	#[cfg(feature = "json-schema")]
+	JsonSchema(treeldr_json_schema::import::Error<source::FileId>),
+}
+
+impl From<syntax::build::Error<source::FileId>> for LangError {
+	fn from(e: syntax::build::Error<source::FileId>) -> Self {
+		Self::TreeLdr(e)
+	}
+}
+
+#[cfg(feature = "json-schema")]
+impl From<treeldr_json_schema::import::Error<source::FileId>> for LangError {
+	fn from(e: treeldr_json_schema::import::Error<source::FileId>) -> Self {
+		Self::JsonSchema(e)
+	}
+}
+
+impl treeldr::reporting::DiagnoseWithVocabulary<source::FileId> for LangError {
+	fn message(&self, vocabulary: &Vocabulary) -> String {
+		match self {
+			Self::TreeLdr(e) => e.message(vocabulary),
+			#[cfg(feature = "json-schema")]
+			Self::JsonSchema(e) => e.message(vocabulary),
+		}
+	}
+
+	fn labels(
+		&self,
+		vocabulary: &Vocabulary,
+	) -> Vec<codespan_reporting::diagnostic::Label<source::FileId>> {
+		match self {
+			Self::TreeLdr(e) => e.labels(vocabulary),
+			#[cfg(feature = "json-schema")]
+			Self::JsonSchema(e) => e.labels(vocabulary),
+		}
+	}
+
+	fn notes(&self, vocabulary: &Vocabulary) -> Vec<String> {
+		match self {
+			Self::TreeLdr(e) => e.notes(vocabulary),
+			#[cfg(feature = "json-schema")]
+			Self::JsonSchema(e) => e.notes(vocabulary),
+		}
+	}
+}
+
+pub enum Document {
+	TreeLdr(Box<TreeLdrDocument>),
+
+	#[cfg(feature = "json-schema")]
+	JsonSchema(Box<treeldr_json_schema::Schema>),
+}
+
+impl Document {
+	/// Load the document located at the given `path`.
+	pub fn load(
+		files: &mut source::Files,
+		filename: &Path
+	) -> Result<(Self, source::FileId), LoadError> {
+		match files.load(&filename, None, None) {
+			Ok(file_id) => {
+				let document = match files.get(file_id).unwrap().mime_type() {
+					Some(source::MimeType::TreeLdr) => {
+						Document::TreeLdr(Box::new(import_treeldr(&files, file_id)))
+					}
+					#[cfg(feature = "json-schema")]
+					Some(source::MimeType::JsonSchema) => {
+						Document::JsonSchema(Box::new(import_json_schema(&files, file_id)))
+					}
+					#[allow(unreachable_patterns)]
+					Some(mime_type) => return Err(LoadError::UnsupportedMimeType(mime_type)),
+					None => return Err(LoadError::UnrecognizedFormat(filename.to_owned()))
+				};
+	
+				Ok((document, file_id))
+			}
+			Err(e) => Err(LoadError::UnableToRead(filename.to_owned(), e))
+		}
+	}
+
+	fn declare(
+		&mut self,
+		context: &mut BuildContext,
+		vocabulary: &mut Vocabulary,
+	) -> Result<(), LangError> {
+		match self {
+			Self::TreeLdr(d) => {
+				d.declare(context, vocabulary)?;
+				Ok(())
+			}
+			#[cfg(feature = "json-schema")]
+			Self::JsonSchema(s) => {
+				treeldr_json_schema::import_schema(s, None, context, vocabulary)?;
+				Ok(())
+			}
+		}
+	}
+
+	fn build(self, context: &mut BuildContext, vocabulary: &mut Vocabulary) -> Result<(), LangError> {
+		match self {
+			Self::TreeLdr(d) => {
+				d.build(context, vocabulary)?;
+				Ok(())
+			}
+			#[cfg(feature = "json-schema")]
+			Self::JsonSchema(_) => Ok(()),
+		}
+	}
+}
+
+/// Import a TreeLDR file.
+fn import_treeldr(files: &source::Files, source_id: source::FileId) -> TreeLdrDocument {
+	use syntax::Parse;
+	use treeldr::reporting::Diagnose;
+	let file = files.get(source_id).unwrap();
+
+	let mut lexer =
+		syntax::Lexer::<_, Infallible, _>::new(source_id, file.buffer().chars().map(Result::Ok));
+
+	log::debug!("ready for parsing.");
+	match syntax::Document::parse(&mut lexer) {
+		Ok(doc) => {
+			log::debug!("parsing succeeded.");
+			TreeLdrDocument {
+				doc: doc.into_value(),
+				local_context: syntax::build::LocalContext::new(
+					file.base_iri().map(|iri| iri.into()),
+				),
+			}
+		}
+		Err(e) => {
+			let diagnostic = e.diagnostic();
+			let writer = StandardStream::stderr(ColorChoice::Always);
+			let config = codespan_reporting::term::Config::default();
+			term::emit(&mut writer.lock(), &config, files, &diagnostic).expect("diagnostic failed");
+			std::process::exit(1);
+		}
+	}
+}
+
+#[cfg(feature = "json-schema")]
+fn import_json_schema(
+	files: &source::Files,
+	source_id: source::FileId,
+) -> treeldr_json_schema::Schema {
+	let file = files.get(source_id).unwrap();
+	let json: serde_json::Value = serde_json::from_str(file.buffer()).expect("invalid JSON");
+	treeldr_json_schema::Schema::try_from(json).expect("invalid JSON Schema")
+}
---
+++ load/src/source.rs
@@ -0,0 +1,240 @@
+use iref::{Iri, IriBuf};
+use std::collections::HashMap;
+use std::fmt;
+use std::ops::{Deref, Range};
+use std::path::{Path, PathBuf};
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord, Debug)]
+pub struct FileId(usize);
+
+pub struct File {
+	source: PathBuf,
+	base_iri: Option<IriBuf>,
+	buffer: Buffer,
+	mime_type: Option<MimeType>,
+}
+
+impl File {
+	pub fn source(&self) -> &Path {
+		&self.source
+	}
+
+	pub fn base_iri(&self) -> Option<Iri> {
+		self.base_iri.as_ref().map(IriBuf::as_iri)
+	}
+
+	pub fn buffer(&self) -> &Buffer {
+		&self.buffer
+	}
+
+	pub fn mime_type(&self) -> Option<MimeType> {
+		self.mime_type
+	}
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+pub enum MimeType {
+	/// application/treeldr
+	TreeLdr,
+
+	/// application/schema+json
+	JsonSchema,
+}
+
+impl MimeType {
+	fn name(&self) -> &'static str {
+		match self {
+			Self::TreeLdr => "application/treeldr",
+			Self::JsonSchema => "application/schema+json",
+		}
+	}
+
+	fn infer(source: &Path, _content: &str) -> Option<MimeType> {
+		source
+			.extension()
+			.and_then(std::ffi::OsStr::to_str)
+			.and_then(|ext| match ext {
+				"tldr" => Some(MimeType::TreeLdr),
+				"json" => Some(MimeType::JsonSchema),
+				_ => None,
+			})
+	}
+}
+
+impl fmt::Display for MimeType {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		self.name().fmt(f)
+	}
+}
+
+#[derive(Default)]
+pub struct Files {
+	files: Vec<File>,
+	sources: HashMap<PathBuf, FileId>,
+}
+
+impl Files {
+	pub fn new() -> Self {
+		Self::default()
+	}
+
+	pub fn get(&self, id: FileId) -> Option<&File> {
+		self.files.get(id.0)
+	}
+
+	// pub fn source(&self, id: FileId) -> Option<&Path> {
+	// 	self.get(id).map(File::source)
+	// }
+
+	// pub fn content(&self, id: FileId) -> Option<&Buffer> {
+	// 	self.get(id).map(File::buffer)
+	// }
+
+	pub fn load(
+		&mut self,
+		source: &impl AsRef<Path>,
+		base_iri: Option<IriBuf>,
+		mime_type: Option<MimeType>,
+	) -> std::io::Result<FileId> {
+		let source = source.as_ref();
+		match self.sources.get(source).cloned() {
+			Some(id) => Ok(id),
+			None => {
+				let content = std::fs::read_to_string(source)?;
+				let id = FileId(self.files.len());
+				let mime_type = mime_type.or_else(|| MimeType::infer(source, &content));
+				self.files.push(File {
+					source: source.into(),
+					base_iri,
+					buffer: Buffer::new(content),
+					mime_type,
+				});
+				self.sources.insert(source.into(), id);
+				Ok(id)
+			}
+		}
+	}
+}
+
+/// Source file buffer.
+///
+/// Stores the file content and lines index.
+pub struct Buffer {
+	/// Buffer data.
+	data: String,
+
+	/// Lines index.
+	line_starts: Vec<usize>,
+}
+
+impl Buffer {
+	/// Creates a new buffer from its content.
+	#[inline(always)]
+	pub fn new(content: String) -> Self {
+		let line_starts = codespan_reporting::files::line_starts(&content).collect();
+
+		Self {
+			data: content,
+			line_starts,
+		}
+	}
+
+	#[inline(always)]
+	fn as_str(&self) -> &str {
+		self.data.as_str()
+	}
+
+	#[inline(always)]
+	pub fn line_count(&self) -> usize {
+		self.line_starts.len()
+	}
+
+	/// The index of the line at the given byte index.
+	///
+	/// If the byte index is past the end of the buffer,
+	/// returns the maximum line index in the file.
+	#[inline(always)]
+	pub fn line_index(&self, byte_index: usize) -> usize {
+		match self.line_starts.binary_search(&byte_index) {
+			Ok(line) => line,
+			Err(next_line) => next_line - 1,
+		}
+	}
+
+	pub fn line_range(&self, line_index: usize) -> Option<Range<usize>> {
+		if line_index < self.line_starts.len() {
+			let range = if line_index + 1 < self.line_starts.len() {
+				self.line_starts[line_index]..self.line_starts[line_index + 1]
+			} else {
+				self.line_starts[line_index]..self.len()
+			};
+
+			Some(range)
+		} else {
+			None
+		}
+	}
+}
+
+impl Deref for Buffer {
+	type Target = str;
+
+	fn deref(&self) -> &str {
+		self.as_str()
+	}
+}
+
+impl AsRef<str> for Buffer {
+	fn as_ref(&self) -> &str {
+		self.as_str()
+	}
+}
+
+impl<'a> codespan_reporting::files::Files<'a> for Files {
+	type FileId = FileId;
+	type Name = std::path::Display<'a>;
+	type Source = &'a Buffer;
+
+	fn name(&'a self, id: FileId) -> Result<Self::Name, codespan_reporting::files::Error> {
+		Ok(self
+			.get(id)
+			.ok_or(codespan_reporting::files::Error::FileMissing)?
+			.source()
+			.display())
+	}
+
+	fn source(&'a self, id: FileId) -> Result<Self::Source, codespan_reporting::files::Error> {
+		Ok(self
+			.get(id)
+			.ok_or(codespan_reporting::files::Error::FileMissing)?
+			.buffer())
+	}
+
+	fn line_index(
+		&'a self,
+		id: FileId,
+		byte_index: usize,
+	) -> Result<usize, codespan_reporting::files::Error> {
+		self.get(id)
+			.map(|file| file.buffer().line_index(byte_index))
+			.ok_or(codespan_reporting::files::Error::FileMissing)
+	}
+
+	fn line_range(
+		&'a self,
+		id: FileId,
+		line_index: usize,
+	) -> Result<Range<usize>, codespan_reporting::files::Error> {
+		self.get(id)
+			.map(|file| {
+				file.buffer().line_range(line_index).ok_or_else(|| {
+					codespan_reporting::files::Error::LineTooLarge {
+						given: line_index,
+						max: file.buffer().line_count(),
+					}
+				})
+			})
+			.transpose()?
+			.ok_or(codespan_reporting::files::Error::FileMissing)
+	}
+}
