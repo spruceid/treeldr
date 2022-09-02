5393a054589f9188531d2d7957d46748e7bbead9
Timothée Haudebourg <timothee.haudebourg@hemisemi.fr>
2022-09-02 11:17:30 +0200

    Upgrade dependencies versions.

--- modules/rust/prelude/src/json_ld.rs
+++ modules/rust/prelude/src/json_ld.rs
@@ -1,32 +1,32 @@
 use crate::rdf::{Literal, Object, Subject};
 use iref::IriBuf;
 use json_ld::ValidReference;
-use rdf_types::Quad;
+use rdf_types::{Quad, BlankIdBuf};
 
-pub trait IntoJsonLd<C, M> {
-	fn into_json_ld(self) -> json_ld::syntax::Value<C, M>;
+pub trait IntoJsonLd<M> {
+	fn into_json_ld(self) -> json_ld::syntax::Value<M>;
 }
 
-impl<C, M> IntoJsonLd<C, M> for bool {
-	fn into_json_ld(self) -> json_ld::syntax::Value<C, M> {
+impl<M> IntoJsonLd<M> for bool {
+	fn into_json_ld(self) -> json_ld::syntax::Value<M> {
 		json_ld::syntax::Value::Boolean(self)
 	}
 }
 
-impl<C, M> IntoJsonLd<C, M> for String {
-	fn into_json_ld(self) -> json_ld::syntax::Value<C, M> {
+impl<M> IntoJsonLd<M> for String {
+	fn into_json_ld(self) -> json_ld::syntax::Value<M> {
 		json_ld::syntax::Value::String(self.into())
 	}
 }
 
-impl<C, M> IntoJsonLd<C, M> for chrono::DateTime<chrono::Utc> {
-	fn into_json_ld(self) -> json_ld::syntax::Value<C, M> {
+impl<M> IntoJsonLd<M> for chrono::DateTime<chrono::Utc> {
+	fn into_json_ld(self) -> json_ld::syntax::Value<M> {
 		json_ld::syntax::Value::String(self.to_string().into())
 	}
 }
 
-impl<I: std::fmt::Display, B: std::fmt::Display, C, M> IntoJsonLd<C, M> for Subject<I, B> {
-	fn into_json_ld(self) -> json_ld::syntax::Value<C, M> {
+impl<I: std::fmt::Display, B: std::fmt::Display, M> IntoJsonLd<M> for Subject<I, B> {
+	fn into_json_ld(self) -> json_ld::syntax::Value<M> {
 		match self {
 			Subject::Iri(i) => json_ld::syntax::Value::String(i.to_string().into()),
 			Subject::Blank(b) => json_ld::syntax::Value::String(b.to_string().into()),
@@ -35,19 +35,14 @@ impl<I: std::fmt::Display, B: std::fmt::Display, C, M> IntoJsonLd<C, M> for Subj
 }
 
 pub fn import_quad<'a>(
-	Quad(subject, predicate, object, graph): json_ld::rdf::QuadRef<'a, IriBuf>,
+	Quad(subject, predicate, object, graph): json_ld::rdf::QuadRef<'a, IriBuf, BlankIdBuf>,
 ) -> Quad<Subject<IriBuf>, Subject<IriBuf>, Object<Subject<IriBuf>>, &'a ValidReference<IriBuf>> {
 	let subject = subject.as_ref().clone().into_rdf_subject();
-
-	let predicate = match predicate {
-		json_ld::rdf::PropertyRef::Other(r) => r.clone().into_rdf_subject(),
-		json_ld::rdf::PropertyRef::Rdf(p) => Subject::Iri(p.as_iri().into()),
-	};
+	let predicate = predicate.as_ref().clone().into_rdf_subject();
 
 	let object = match object {
 		json_ld::rdf::Value::Literal(l) => Object::Literal(import_literal(l)),
-		json_ld::rdf::Value::Reference(r) => Object::Id(r.into_rdf_subject()),
-		json_ld::rdf::Value::Nil => Object::Id(Subject::Iri(json_ld::rdf::NIL_IRI.into())),
+		json_ld::rdf::Value::Reference(r) => Object::Id(r.into_rdf_subject())
 	};
 
 	Quad(subject, predicate, object, graph)
@@ -57,17 +52,8 @@ pub fn import_literal(lit: json_ld::rdf::Literal<IriBuf>) -> Literal<Subject<Iri
 	match lit {
 		json_ld::rdf::Literal::String(s) => Literal::String(s),
 		json_ld::rdf::Literal::TypedString(s, ty) => {
-			Literal::TypedString(s, import_literal_type(ty))
+			Literal::TypedString(s, Subject::Iri(ty))
 		}
 		json_ld::rdf::Literal::LangString(s, l) => Literal::LangString(s, l),
-	}
-}
-
-pub fn import_literal_type(ty: json_ld::rdf::LiteralType<IriBuf>) -> Subject<IriBuf> {
-	match ty {
-		json_ld::rdf::LiteralType::Rdfs(ty) => Subject::Iri(ty.as_iri().into()),
-		json_ld::rdf::LiteralType::Xsd(ty) => Subject::Iri(ty.as_iri().into()),
-		json_ld::rdf::LiteralType::I18n(ty) => Subject::Iri(ty.as_iri().into()),
-		json_ld::rdf::LiteralType::Other(iri) => Subject::Iri(iri),
 	}
 }
